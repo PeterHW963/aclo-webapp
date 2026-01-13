fd7cae52b151b90a4c64a590ad4043bb59d03bdc
celine <josephinecelinetj@gmail.com>
2026-01-13 17:36:28 +0800

    Remove unused code

--- backend/models/Checkout.js
+++ backend/models/Checkout.js
@@ -119,16 +119,6 @@ const checkoutSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-// checkoutSchema.pre("validate", async function (next) {
-//     if (this.orderId) return next();
-//     try {
-//         this.orderId = await generateOrderId();
-//         next();
-//     } catch (e) {
-//         next(e);
-//     }
-// });
-
 checkoutSchema.pre("validate", async function () {
     if (this.orderId) return;
     this.orderId = await generateOrderId();
