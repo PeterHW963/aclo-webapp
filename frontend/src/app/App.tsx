b337836e8151747cc39dee7580cc22adfaa69b11
celine <josephinecelinetj@gmail.com>
2026-02-22 03:44:59 +0800

    Add subscriber/ newsletter pop up

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 import { Provider } from "react-redux";
 
@@ -32,63 +32,75 @@ import Story from "../features/landing/pages/Story";
 import Contact from "../features/landing/pages/Contact";
 import LearnMore from "../features/landing/pages/LearnMore";
 import ScrollToTop from "../shared/utils/scrollToTop";
+import NewsletterPopup from "../shared/components/common/NewsletterPopup";
+
+function AppInner() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
+  return (
+    <>
+      <ScrollToTop />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: { fontSize: "20px", padding: "20px 30px" },
+        }}
+      />
+
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/verified" element={<Verified />} />
+          <Route path="story" element={<Story />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="learn-more" element={<LearnMore />} />
+          <Route path="shop" element={<ShopPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout/:cartId" element={<Checkout />} />
+          <Route path="payment/:checkoutId" element={<Payment />} />
+          <Route
+            path="order/:orderId/confirmation"
+            element={<OrderConfirmation />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="my-orders" element={<MyOrdersPage />} />
+        </Route>
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route
+            path="products/:id/edit/:variantId"
+            element={<EditProductPage />}
+          />
+          <Route path="orders" element={<OrderManagement />} />
+        </Route>
+      </Routes>
+
+      {isHome && <NewsletterPopup key={location.key} />}
+    </>
+  );
+}
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <ScrollToTop />
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            style: { fontSize: "20px", padding: "20px 30px" },
-          }}
-        />
-        <Routes>
-          <Route path="/" element={<UserLayout />}>
-            {/* User Layout */}
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/verified" element={<Verified />} />
-
-            <Route path="story" element={<Story />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="learn-more" element={<LearnMore />} />
-            {/* anything after a colon is a dynamic route */}
-            <Route path="shop" element={<ShopPage />} />
-            <Route path="product/:id" element={<ProductDetails />} />
-            <Route path="checkout/:cartId" element={<Checkout />} />
-            <Route path="payment/:checkoutId" element={<Payment />} />
-            <Route
-              path="order/:orderId/confirmation"
-              element={<OrderConfirmation />}
-            />
-            <Route path="order/:id" element={<OrderDetailsPage />} />
-            <Route path="my-orders" element={<MyOrdersPage />} />
-          </Route>
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute role="admin">
-                <AdminLayout />
-              </ProtectedRoute>
-            }
-          >
-            {/* Admin Layout */}
-            <Route index element={<AdminHomePage />} />
-            <Route path="users" element={<UserManagement />} />
-            <Route path="products" element={<ProductManagement />} />
-            <Route
-              path="products/:id/edit/:variantId"
-              element={<EditProductPage />}
-            />
-            <Route path="orders" element={<OrderManagement />} />
-          </Route>
-        </Routes>
+        <AppInner />
       </BrowserRouter>
     </Provider>
   );
